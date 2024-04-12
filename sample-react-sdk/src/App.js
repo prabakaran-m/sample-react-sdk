4ade9cbe70c20194913a34edd637379d7e3e77b5
Prabakaran Muthusamy <prabakaran.m@kgisl.com>
2024-04-12 17:43:04 +0530

    Added files

---
+++ sample-react-sdk/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Sample React SDK</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Here is the sample NPM Package for testing.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
